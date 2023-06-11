1ded53ee2658c16abd755629555e531be0773256
tlsyfk <tlsyfk@naver.com>
2023-06-11 20:30:51 +0900

    initial commit

--- pybo/views.py
+++ pybo/views.py
@@ -4,11 +4,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get('page', '1')  # 페이지
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 # Create your views here.
 
